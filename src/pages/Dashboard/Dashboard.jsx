9d87c523d39666cf4a29ec5ef5032eb738e2e650
Aslam Parvej <aslamparvej2@gmail.com>
2026-04-15 17:57:12 +0530

    Working on Dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,20 @@
-import { useIdeas } from "../../lib/context/project";
+import { useProjects } from "../../lib/context/projects";
+import Loading from "../Loading";
 import "./Dashboard.css";
 
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
 const Dashboard = () => {
-  const projects = useIdeas();
+  const { projects, loading } = useProjects();
 
+  if (loading) return <Loading />;
 
-  return <div className="dashboard-container">
-    <h2>Project {projects.length}</h2>
-  </div>;
+  return (
+    <div className="dashboard-container">
+      <h2>Project {projects.length}</h2>
+    </div>
+  );
 };
 
 export default Dashboard;
